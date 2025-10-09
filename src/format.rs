b5dc4a53a64307dba5155c56f1c71560cbd26c12
vedant-asati03 <vedant.asati03@gmail.com>
2025-10-09 12:10:26 +0530

    Remove unused console and format utility files

--- src/format.rs
+++
@@ -1,7 +0,0 @@
-//! Output formatting utilities
-
-// TODO: Implement various output formatters:
-// - JSON formatter with pretty printing options
-// - Structured text formatter
-// - Binary format for high performance
-// - Custom format templates
